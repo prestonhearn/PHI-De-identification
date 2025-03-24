c7fdde0e494878615076dd7efb4ff67d3c98a90d
Farhin-Rashid <farhinalavirashid@gmail.com>
2025-03-23 22:56:49 -0500

    Updated dob regex for readability

--- src/Recognizer.py
+++ src/Recognizer.py
@@ -24,7 +24,23 @@ class AddressRecognizer(PatternRecognizer):
     
 class DOBRecognizer(PatternRecognizer):
     def __init__(self):
-        patterns = [Pattern("DOB", r"(?i)(date of birth|dob|birth date)\s?:?\s?(\d{1,2}\/\d{1,2}\/\d{4}|\d{4}\/\d{1,2}\/\d{1,2}|\d{1,2}-\d{1,2}-\d{4}|\d{1,2}\.\d{1,2}\.\d{4}|\d{4}\.\d{1,2}\.\d{1,2}|\d{4}-\d{1,2}-\d{1,2}|\d{8}|\d{1,2}\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{4}|(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{1,2},\s\d{4}|\d{1,2}\s(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{4}|(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{1,2},\s\d{4})", score=1.0)]
+        patterns = [Pattern(
+            "DOB",
+            r"""(?i)(date of birth|dob|birth date)\s?:?\s?(
+                \d{1,2}/\d{1,2}/\d{4}|
+                \d{4}/\d{1,2}/\d{1,2}|
+                \d{1,2}-\d{1,2}-\d{4}|
+                \d{1,2}\.\d{1,2}\.\d{4}|
+                \d{4}\.\d{1,2}\.\d{1,2}|
+                \d{4}-\d{1,2}-\d{1,2}|
+                \d{8}|
+                \d{1,2}\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{4}|
+                (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{1,2},\s\d{4}|
+                \d{1,2}\s(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{4}|
+                (January|February|March|April|May|June|July|August|September|October|November|December)\s\d{1,2},\s\d{4}
+            )""",
+            score=1.0,
+        )]
 
         super().__init__(supported_entity="DOB", patterns=patterns)
 
