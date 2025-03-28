2588e135c1f3b286aa0c32f0bb6e9994e6cdee8f
Preston <hearnpreston@gmail.com>
2025-03-28 17:56:07 -0500

    added new recognizers to main. cleaned up main.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 import os
-from Recognizer import AddressRecognizer, DOBRecognizer, TitleRecognizer, PostNominalRecognizer, SSNRecognizer, MedicaidAccountRecognizer
+from Recognizer import (
+    AddressRecognizer,
+    DOBRecognizer,
+    TitleRecognizer,
+    PostNominalRecognizer,
+    SSNRecognizer,
+    MedicaidAccountRecognizer,
+    AllergiesRecognizer,
+    LabResultsRecognizer
+)
 from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import OperatorConfig
@@ -11,46 +20,56 @@ file_path = [os.path.join(".", "src", "resources", "ehr JMS.txt"),
 analyzer = AnalyzerEngine()
 anonymizer = AnonymizerEngine()
 
-address_rec = AddressRecognizer()
-dob_rec = DOBRecognizer()
-title_rec = TitleRecognizer() 
-postnominal_rec = PostNominalRecognizer()
-ssn_rec = SSNRecognizer()
-medicaid_rec = MedicaidAccountRecognizer()
+recognizers = [
+    AddressRecognizer(),
+    DOBRecognizer(),
+    TitleRecognizer(),
+    PostNominalRecognizer(),
+    SSNRecognizer(),
+    MedicaidAccountRecognizer(),
+    AllergiesRecognizer(),
+    LabResultsRecognizer()
+]
 
-
-analyzer.registry.add_recognizer(address_rec)
-analyzer.registry.add_recognizer(dob_rec)
-analyzer.registry.add_recognizer(title_rec)
-analyzer.registry.add_recognizer(postnominal_rec)
-analyzer.registry.add_recognizer(ssn_rec)
-analyzer.registry.add_recognizer(medicaid_rec)
-
-#"./resources/ehr JMS.txt"
+for recognizer in recognizers:
+    analyzer.registry.add_recognizer(recognizer)
 
 for file_path in file_path:
     with open(file_path, 'r') as file:
         content = file.read()
 
-    results = analyzer.analyze(text=content,
-                            entities=["PERSON", "ADDRESS", "DOB", "SSN", "PHONE_NUMBER", "EMAIL_ADDRESS", "TITLE", "POSTNOMINAL", "MEDICAID_ACCOUNT"],
-                            language='en')
+    entities = [
+        "PERSON", "ADDRESS", "DOB", "SSN", "PHONE_NUMBER", 
+        "EMAIL_ADDRESS", "TITLE", "POSTNOMINAL", "MEDICAID_ACCOUNT",
+        "ALLERGIES", "LAB_RESULTS"
+    ]
+
+    operator_config = {
+        "PERSON": OperatorConfig("replace", {"new_value": "*name*"}),
+        "ADDRESS": OperatorConfig("replace", {"new_value": "*address*"}),
+        "TITLE": OperatorConfig("replace", {"new_value": "*title*"}),
+        "DOB": OperatorConfig("replace", {"new_value": "\n*dob*"}),
+        "SSN": OperatorConfig("replace", {"new_value": "*ssn*"}),
+        "PHONE_NUMBER": OperatorConfig("replace", {"new_value": "*phone*"}),
+        "POSTNOMINAL": OperatorConfig("replace", {"new_value": "*pn*"}),
+        "EMAIL_ADDRESS": OperatorConfig("replace", {"new_value": "*email*"}),
+        "MEDICAID_ACCOUNT": OperatorConfig("replace", {"new_value": "*medicaid*"}),
+        "ALLERGIES": OperatorConfig("replace", {"new_value": "*allergies*\n\n"}),
+        "LAB_RESULTS": OperatorConfig("replace", {"new_value": "*lab results*\n\n"})
+    }
+
+    results = analyzer.analyze(
+        text=content,
+        entities=entities,
+        language='en'
+    )
 
     result = anonymizer.anonymize(
         text=content,
         analyzer_results=results,
-        operators={"PERSON": OperatorConfig("replace", {"new_value": "*name*"}),
-                "ADDRESS": OperatorConfig("replace", {"new_value": "*address*"}),
-                "TITLE": OperatorConfig("replace",  {"new_value": "*title*"}),
-                "DOB": OperatorConfig("replace", {"new_value": ("\nDate of Birth: " + "*dob*")}),
-                "SSN": OperatorConfig("replace", {"new_value": "*ssn*"}), 
-                "PHONE_NUMBER": OperatorConfig("replace", {"new_value": "*phone*"}), 
-                "POSTNOMINAL": OperatorConfig("replace", {"new_value": "*pn*"}), 
-                "EMAIL_ADDRESS": OperatorConfig("replace", {"new_value": "*email*"}),
-                "MEDICAID_ACCOUNT": OperatorConfig("replace", {"new_value": "*medicaid*"}),
-                }
-
+        operators=operator_config
     )
+    
     # Generate unique filenames for each processed file
     base_filename = os.path.splitext(os.path.basename(file_path))[0]
 
