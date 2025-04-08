75525ce1ad78ebf3f6e6e4fc4562a3e32369c3a5
Preston <hearnpreston@gmail.com>
2025-04-08 12:32:47 -0500

    refactored main to handle ehr3 separately added "dates" to ehr3 analyzer

--- src/main.py
+++ src/main.py
@@ -16,9 +16,10 @@ from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import OperatorConfig
 
+ehr3_filepath = os.path.join(".", "src", "resources", "ehr EC 3 .txt")
 file_path = [os.path.join(".", "src", "resources", "ehr JMS.txt"),
              os.path.join(".", "src", "resources", "ehr MH 2.txt"),
-             os.path.join(".", "src", "resources", "ehr EC 3 .txt")
+             ehr3_filepath
 ]
 
 analyzer = AnalyzerEngine()
@@ -44,22 +45,30 @@ for recognizer in recognizers:
 for file_path in file_path:
     with open(file_path, 'r') as file:
         content = file.read()
-
-    entities = [
-        "PERSON", "ADDRESS", "DOB", "SSN", "PHONE_NUMBER", 
-        "EMAIL_ADDRESS", "TITLE", "POSTNOMINAL", "MEDICAID_ACCOUNT",
-        "ALLERGIES", "LAB_RESULTS", "HOSPITAL", "FAX", "WEB_URL", "IP_ADDRESS"
-    ]
+    
+    if file_path == ehr3_filepath:
+        entities = [
+            "TITLE", "PERSON", "POSTNOMINAL", "ADDRESS", "DOB",
+            "DATE_TIME", "PHONE_NUMBER", "FAX", "EMAIL_ADDRESS",
+            "SSN",  "MEDICAID_ACCOUNT", "WEB_URL", "IP_ADDRESS"
+        ]
+    else:
+        entities = [
+            "PERSON", "ADDRESS", "DOB", "SSN", "PHONE_NUMBER", 
+            "EMAIL_ADDRESS", "TITLE", "POSTNOMINAL", "MEDICAID_ACCOUNT",
+            "ALLERGIES", "LAB_RESULTS", "HOSPITAL", "IP_ADDRESS"
+        ]
 
     operator_config = {
-        "PERSON": OperatorConfig("replace", {"new_value": "*name*"}),
-        "ADDRESS": OperatorConfig("replace", {"new_value": "*address*"}),
         "TITLE": OperatorConfig("replace", {"new_value": "*title*"}),
+        "PERSON": OperatorConfig("replace", {"new_value": "*name*"}),
+        "POSTNOMINAL": OperatorConfig("replace", {"new_value": "*pn*"}),            
+        "ADDRESS": OperatorConfig("replace", {"new_value": "*address*"}),
+        "DATE_TIME": OperatorConfig("replace", {"new_value": "*date*"}),
         "DOB": OperatorConfig("replace", {"new_value": "\n*dob*"}),
-        "SSN": OperatorConfig("replace", {"new_value": "*ssn*"}),
         "PHONE_NUMBER": OperatorConfig("replace", {"new_value": "*phone*"}),
-        "POSTNOMINAL": OperatorConfig("replace", {"new_value": "*pn*"}),
         "EMAIL_ADDRESS": OperatorConfig("replace", {"new_value": "*email*"}),
+        "SSN": OperatorConfig("replace", {"new_value": "*ssn*"}),
         "MEDICAID_ACCOUNT": OperatorConfig("replace", {"new_value": "*medicaid*"}),
         "ALLERGIES": OperatorConfig("replace", {"new_value": "*allergies*\n\n"}),
         "LAB_RESULTS": OperatorConfig("replace", {"new_value": "*lab results*\n\n"}),
@@ -68,6 +77,7 @@ for file_path in file_path:
         "WEB_URL": OperatorConfig("replace", {"new_value": "*url*"}),
         "IP_ADDRESS": OperatorConfig("replace", {"new_value": "*ip*"})
     }
+    
 
     results = analyzer.analyze(
         text=content,
